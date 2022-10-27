589b2e1ccb01dc3e7d88d890f86db576112fb5a9
ayo oke <haywee39@gmail.com>
2022-10-27 04:03:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+$(document).ready(function(){
+    $('#burger').click(function(){
+        $('#head').slideToggle('1000',function(){
+            $('#burger').hide();
+            $('#times').show();
+        }) 
+    })
+
+    $('#times').click(function(){
+        $('#head').slideToggle('10000', function(){
+            $('#times').hide();
+            $('#burger').show()
+        })
+
+    })
+   
+   
+})
+
+
+
