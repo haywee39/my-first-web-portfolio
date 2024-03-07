c7ae24caea3df8758a903b695f73a4f5698af337
[Ayodipupo Oke] <[haywee39@gmail.com]>
2024-03-07 13:07:32 +0100

    home redesign start

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 
 
-$(document).ready(function(){
-    $('#burger').click(function(){
-        $('#head').slideToggle('1000',function(){
-            $('#burger').hide();
-            $('#times').show();
-        }) 
-    })
+// $(document).ready(function(){
+//     $('#burger').click(function(){
+//         $('#head').slideToggle('1000',function(){
+//             $('#burger').hide();
+//             $('#times').show();
+//         }) 
+//     })
 
-    $('#times').click(function(){
-        $('#head').slideToggle('10000', function(){
-            $('#times').hide();
-            $('#burger').show()
-        })
+//     $('#times').click(function(){
+//         $('#head').slideToggle('10000', function(){
+//             $('#times').hide();
+//             $('#burger').show()
+//         })
 
-    })
+//     })
    
    
-})
+// })
 
 
 
